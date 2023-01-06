852840cabe8335718bb14e0ab9315fbf32c81712
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2023-01-06 06:07:25 -0800

    main : task 2 added for card 1

---
+++ card1/task2.sql
@@ -0,0 +1,19 @@
+USE [БД.ПО4.190333-билет1];
+GO
+
+DROP PROCEDURE IF EXISTS Билет1_Задание2;
+GO
+
+CREATE PROCEDURE Билет1_Задание2 @text VARCHAR(64) AS
+BEGIN
+	DECLARE @searchText VARCHAR(100);
+
+	SET @searchText = CONCAT('%', @text,'!%%'); 
+
+	SELECT * FROM СПР_Лекарства
+	WHERE наименование LIKE @searchText ESCAPE '!'
+END;
+GO
+
+EXEC Билет1_Задание2 @text = '3';
+GO
